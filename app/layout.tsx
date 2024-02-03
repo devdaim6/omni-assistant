97bab6defae82d283acef9aa21d9a9a2906661bc
devdaim6 <daimdev6@gmail.com>
2024-02-03 11:37:46 +0530

    Add Nextjs Top Loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Nav } from "@/components/Nav";
 import { AuthProvider, NextProvider } from "./Providers";
 import "./globals.css";
 import { Toaster } from "sonner";
-
+import NextTopLoader from "nextjs-toploader";
 export type Metadata = {
   title: string;
   description: string;
@@ -29,7 +29,25 @@ export default function RootLayout({
           <NextProvider>
             <Toaster position="bottom-center" closeButton duration={2000} />
             <Nav />
-            <section className="p-4 pt-16 ">{children}</section> <Footer />
+            <section className="p-4 pt-16 ">
+              <NextTopLoader
+                color="#2299DD"
+                initialPosition={0.08}
+                crawlSpeed={200}
+                height={3}
+                crawl={true}
+                showSpinner={true}
+                easing="ease"
+                speed={200}
+                shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+                template='<div class="bar" role="bar"><div class="peg"></div></div> 
+                <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+                zIndex={1600}
+                showAtBottom={false}
+              />
+              {children}
+            </section>{" "}
+            <Footer />
           </NextProvider>
         </AuthProvider>
       </body>
